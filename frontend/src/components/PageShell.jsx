507fbc5373891281c0a4d18e5a19323f333899fa
Karan <prokaranvir@gmail.com>
2026-03-20 03:06:25 -0400

    feat: add shared page shell

---
+++ frontend/src/components/PageShell.jsx
@@ -0,0 +1,18 @@
+import HeaderBar from "./HeaderBar";
+import Sidebar from "./Sidebar";
+
+function PageShell({ children }) {
+  return (
+    <div className="app-shell">
+      <Sidebar />
+
+      <main className="main-panel">
+        <HeaderBar />
+
+        <div className="content-card">{children}</div>
+      </main>
+    </div>
+  );
+}
+
+export default PageShell;
